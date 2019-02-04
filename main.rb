eaecb8723e4f619c0228a0f068f9eb7f0d4b2db5
Muhammadreza Haghiri <haghiri75@gmail.com>
2019-02-04 13:15:27 +0330

    JSON output added

---
+++ main.rb
@@ -0,0 +1,29 @@
+require 'json'
+require 'mongoid'
+require 'sinatra' 
+
+configure do 
+    Mongoid.load!("./database.yml")
+end 
+
+class Environment 
+    include Mongoid::Document 
+    field :username, type: String 
+    field :temperature, type: String 
+    field :humidity, type: String
+end 
+
+get '/' do 
+    'please use GET /env/:temp/:humid to write and /env/guest to get the data' 
+end 
+
+get '/env/:temp/:humid' do 
+    env = Environment.create(:username => "guest", :temperature => params[:temp], :humidity => params[:humid])
+end 
+
+get '/env/:username' do 
+    content_type :json 
+
+    env = Environment.where(username: "guest") 
+    env = env.to_json 
+end
